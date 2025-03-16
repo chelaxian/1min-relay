55282a93fda9cb19cd18f0e715247cfac544fcb6
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-03-16 17:05:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -176,13 +176,13 @@ ALL_ONE_MIN_AVAILABLE_MODELS = [
 vision_supported_models = [
     "gpt-4o",
     "gpt-4o-mini",
-    "gpt-4-turbo",
-    "claude-3-5-sonnet-20240620",
-    "claude-3-opus-20240229",
-    "claude-3-sonnet-20240229",
-    "claude-3-haiku-20240307",
-    "gemini-1.5-pro",
-    "gemini-1.5-flash"
+    "gpt-4-turbo" #,
+    #"claude-3-5-sonnet-20240620",
+    #"claude-3-opus-20240229",
+    #"claude-3-sonnet-20240229",
+    #"claude-3-haiku-20240307",
+    #"gemini-1.5-pro",
+    #"gemini-1.5-flash"
 ]
 
 # Define models that support tool use (function calling)
