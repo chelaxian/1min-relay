9713e0fea403b94e1af039581317533437c057ba
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-03-16 21:16:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -144,14 +144,13 @@ ALL_ONE_MIN_AVAILABLE_MODELS = [
     "mistral-small-latest",
     "mistral-nemo",
     "open-mistral-7b",
-
-    #"nova", 
-    #"alloy", 
-    #"echo", 
-    #"fable", 
-    #"onyx", 
-    #"shimmer"
     
+    # STT
+    #"whisper-1", 
+
+    # TTS
+    #"alloy", 
+
    # Replicate
    "meta/llama-2-70b-chat", 
    "meta/meta-llama-3-70b-instruct", 
@@ -186,12 +185,11 @@ tools_supported_models = [
 
 # Define models that support text-to-speech
 tts_supported_models = [
-#     "nova", 
-#     "alloy", 
-#     "echo", 
-#     "fable", 
-#     "onyx", 
-#     "shimmer"
+#     "alloy"
+]
+
+stt_supported_models = [
+#     "whisper-1"
 ]
 
 # Define models that support web search
